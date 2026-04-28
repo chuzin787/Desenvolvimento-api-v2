f0168b53d68cbcba5db748ec324b8eee43796d4f
João Rafael Da Silva Fortes <145729921+chuzin787@users.noreply.github.com>
2026-04-28 16:17:12 -0300

    AFFS VEY
    ajeitando tudo pq eu fiquei puto

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/UserRoutes.js'
 
 const app = express()
 
--- api/src/models/userModel.js
+++ api/src/models/userModel.js
@@ -1,12 +1,9 @@
-export const getUsers = async () => {
-  try {
-    const result = await pool.query('SELECT * FROM users')
-    return result.rows
-  } catch (err) {
-    console.error(err)
-    throw err
-  }
-}
-if (!name || !email) {
-  return res.status(400).json({ error: 'Name e email são obrigatórios' })
+import { pool } from '../config/db.js'
+
+export const createUser = async (name, email) => {
+  const result = await pool.query(
+    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
+    [name, email]
+  )
+  return result.rows[0]
 }
