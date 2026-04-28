6a04a64b23d9ecc712769c28b332a66f6a872822
João Rafael Da Silva Fortes <145729921+chuzin787@users.noreply.github.com>
2026-04-28 15:59:44 -0300

    banco
    resolvendo

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -9,5 +9,5 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT), // 👈 aqui sim!
 })
--- api/src/models/userModel.js
+++ api/src/models/userModel.js
@@ -1,14 +1,9 @@
-import { pool } from '../config/db.js'
-
 export const getUsers = async () => {
-  const result = await pool.query('SELECT * FROM users')
-  return result.rows
-}
-
-export const createUser = async (name, email) => {
-  const result = await pool.query(
-    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
-    [name, email]
-  )
-  return result.rows[0]
+  try {
+    const result = await pool.query('SELECT * FROM users')
+    return result.rows
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
 }
