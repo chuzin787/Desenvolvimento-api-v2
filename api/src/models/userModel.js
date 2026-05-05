582aeea535f586a7937622a6c1540b38dda0acdf
João Rafael Da Silva Fortes <145729921+chuzin787@users.noreply.github.com>
2026-05-05 15:27:36 -0300

    Rotas update
    melhorando pq estava dando problema na rota

--- api/src/models/userModel.js
+++ api/src/models/userModel.js
@@ -1,9 +1,6 @@
 import { pool } from '../config/db.js'
 
-export const createUser = async (name, email) => {
-  const result = await pool.query(
-    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
-    [name, email]
-  )
-  return result.rows[0]
+export const getUsers = async () => {
+  const result = await pool.query('SELECT * FROM users')
+  return result.rows
 }
